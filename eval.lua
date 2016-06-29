bc043b5c566f255c0dc4e768f6b1bc920bb26fb6
mtanana <mike.tanana@trivergia.com>
2016-06-28 20:01:37 -0600

    Fixed bugs

--- eval.lua
+++ eval.lua
@@ -2,25 +2,32 @@ require 'neuralconvo'
 require 'util.Tester'
 local tokenizer = require "tokenizer"
 local list = require "pl.List"
+require 'nn'
+local WordSplitLMMinibatchLoader = require 'util.WordSplitLMMinibatchLoader'
+
+
 local options = {}
 
-if dataset == nil then
+if loader == nil then
   cmd = torch.CmdLine()
   cmd:text('Options:')
-  cmd:option('--cuda', true, 'use CUDA. Training must be done on CUDA')
-  cmd:option('--debug', true, 'show debug info')
+  cmd:option('--cuda', false, 'use CUDA. Training must be done on CUDA')
+  cmd:option('--debug', false, 'show debug info')
   cmd:option('--dataset', "model.t7", 'show debug info')
+  cmd:option('--vocablocation', "data/opensubssmall/vocabwords.t7", 'show debug info')
   cmd:text()
   options = cmd:parse(arg)
 
-  -- Data
-  dataset = neuralconvo.DataSet()
 
   -- Enabled CUDA
   if options.cuda then
     require 'cutorch'
     require 'cunn'
   end
+
+  -- Data
+  loader = WordSplitLMMinibatchLoader.createFromJustVocab(options.vocablocation)
+
 end
 
 if model == nil then
@@ -31,7 +38,7 @@ end
 
 
 function say(text)
-  print(getResponse(text,dataset,model,true))
+  print(getResponse(text,loader,model,options.debug))
 end
 
 repeat
